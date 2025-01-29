e5a1b0ee9559f5ad0b3ca1227135ff035f4ab33f
Donald <donumeh54@gmail.com>
2025-01-29 09:54:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# stage_zero
---
+++ app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+
+A python program that retrieves basic information about me
+
+"""
+
+
+from flask import Flask, Response
+from flask_cors import CORS
+from datetime import datetime
+import json
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/", strict_slashes=True)
+def details():
+    details = {
+        "email": "donumeh54@gmail.com",
+        "current_datetime": datetime.now().isoformat(),
+        "github_url": "https://github.com/donumeh/stage_zero",
+    }
+    response = Response(
+        response=json.dumps(details), status=200, mimetype="application/json"
+    )
+    return response
+
+
+if __name__ == "__main__":
+    app.run()
